cdc082915e156757d189fb0d46f96cab4584e468
bayramsonmez510-tech <bayramsonmez510@gmail.com>
2026-03-05 16:57:46 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:57:26.989Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T13:57:45.648Z
+Commit: Fix CSS styling bug on mobile devices
