c2ed6fffd4ba646d732862276bd568f842a91b49
bayramsonmez510-tech <bayramsonmez510@gmail.com>
2026-03-05 17:04:42 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:04:35.873Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T14:04:41.664Z
+Commit: Fix broken links in documentation
