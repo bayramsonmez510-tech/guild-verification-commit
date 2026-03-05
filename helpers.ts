6717dc6b3ca545f3465afedf117781c028519b25
bayramsonmez510-tech <bayramsonmez510@gmail.com>
2026-03-05 17:03:26 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:02:36.078Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T14:03:25.754Z
+Commit: Correct typo in configuration file
