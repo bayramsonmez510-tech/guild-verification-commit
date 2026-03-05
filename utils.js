5320a53948038ea5167b0ae6a598b85ceb5dd89b
bayramsonmez510-tech <bayramsonmez510@gmail.com>
2026-03-05 16:59:34 +0300

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:58:35.193Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T13:59:34.085Z
+Commit: Introduce real-time notifications
