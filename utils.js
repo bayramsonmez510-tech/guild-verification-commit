3a77c199ca29ff6163e1625295da97c743f0e912
bayramsonmez510-tech <bayramsonmez510@gmail.com>
2026-03-05 17:01:51 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:01:41.225Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T14:01:51.028Z
+Commit: Fix broken links in documentation
