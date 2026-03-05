a49d548056a2a7baf927d098b5ebd5c17c52e376
bayramsonmez510-tech <bayramsonmez510@gmail.com>
2026-03-05 16:58:35 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:58:20.265Z
-Commit: Add multi-language support
+Timestamp: 2026-03-05T13:58:35.193Z
+Commit: Resolve null pointer exception in user service
